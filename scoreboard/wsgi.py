28062f3fbd3e771478dda46c93278f3fa7d72183
Steve Walsh <steve@securebroadcast.com>
2015-02-15 19:49:38 +0000

    Change to wsgi.py

--- scoreboard/wsgi.py
+++ scoreboard/wsgi.py
@@ -11,4 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scoreboard.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
+
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
