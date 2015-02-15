fd4150243d061ef92dd54b378c8fdd88ea294430
Steve Walsh <steve@securebroadcast.com>
2015-02-15 22:31:42 +0000

    found some overriding db config

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -65,12 +65,12 @@ WSGI_APPLICATION = 'scoreboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
